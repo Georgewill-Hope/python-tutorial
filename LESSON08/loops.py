62b4d49511417f2e3324c0b384487226a65fad33
Georgewill Hope <georgeblue222@gmail.com>
2026-07-21 00:28:51 +0100

    covered basics of loops and upgraded rock, paper and scissors game

---
+++ LESSON08/loops.py
@@ -0,0 +1,55 @@
+# value = 1 
+# while value <= 10:
+#     print(value)
+#     if(value == 5):
+#         break
+#     value += 1
+
+# value = 1 
+# while value <= 10:
+#     value += 1
+#     if(value == 5):
+#         continue
+#     print(value)
+# else:
+#     print("Value is now equal to " + str(value))
+
+names= ["Dave", "Sarah", "John"]
+
+# for name in names:
+#     print(name)
+
+# for x in "Mississipi":
+#     print(x)
+
+# for name in names:
+#     if(name == "Sarah"):
+#         break
+#     print(name)
+
+# for name in names:
+#     if(name == "Sarah"):
+#         continue
+#     print(name)
+
+# for x in range(4):
+#     print(x)
+
+# for x in range(2, 4):
+#     print(x)
+
+# for x in range(5, 101, 5):
+#     print(x)
+# else:
+#     print("Glad that\'s over!")
+
+# names= ["Dave", "Sarah", "John"]
+# actions = ["codes", "eats", "sleeps"]
+
+# for name in names:
+#     for action in actions:
+#         print(name + " " + action)
+
+# for action in actions:
+#     for name in names:
+#         print(name + " " + action)
