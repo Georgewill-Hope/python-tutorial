926d0339b832628ebb6f0808ce9c9f4fa6199ac7
Georgewill Hope <georgeblue222@gmail.com>
2026-07-21 02:19:51 +0100

    covered scopes in python and also used it to refactor our rock paper and scissors game

---
+++ LESSON11/scope.py
@@ -0,0 +1,18 @@
+name = "Hope"
+count = 1
+
+def another():
+    color = "blue"
+    global count 
+    count += 1
+    print(count)
+
+    def greeting(name):
+        nonlocal color
+        color = "red"
+        print(color)
+        print(name)
+
+    greeting("dave")
+
+another()
