3356ba0b645c17f5badbf4fa5ab9a1e7169fe383
Georgewill Hope <georgeblue222@gmail.com>
2026-07-21 01:52:59 +0100

    covered basics of functions and recursive functions. also upgraded the rps game using recursion

---
+++ LESSON09/functions.py
@@ -0,0 +1,25 @@
+def hello_world():
+    print("Hello World!")
+
+hello_world()
+
+def sum(num1=0, num2=0):
+    if (type(num1) is not int or type(num2) is not int):
+        return 0
+    return num1 + num2
+
+total = sum(2,5)
+print(total)
+
+def multiple_items(*args):
+    print(args)
+    print(type(args))
+
+multiple_items("dave", "john", "sarah")
+
+def mult_named_items(**kwargs):
+    print(kwargs)
+    print(kwargs.keys())
+    print(type(kwargs))
+
+mult_named_items(first="hope", last="georgewill")
